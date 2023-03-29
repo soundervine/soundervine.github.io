7adc96b570d480f869d918e36962945b8e196daa
Jahi Grant <jahigrant@googlemail.com>
2023-03-29 20:08:41 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+document.querySelectorAll('.be-logo-bold').forEach((node) => {
+    node.innerHTML= `<img class="mb-2" src="assets/be-logo.svg" width="32" height="32">`
+});
+
 document.querySelectorAll('.be-logo').forEach((node) => {
-    node.innerHTML= `<img src="assets/be-logo.svg" width="32" height="32">`
+    node.innerHTML= `<img class="mb-0" src="assets/be-logo.svg" width="24" height="24">`
+});
+
+document.querySelectorAll('.ma-logo-plain').forEach((node) => {
+    node.innerHTML= `<img class="pb-2" src="assets/malarkes-logo-plain.svg" height="28">`
 });
